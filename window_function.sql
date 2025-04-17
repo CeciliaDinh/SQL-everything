a6a19bfb08bd7fe7b664f7f84021e38e4f6de13c
CeciliaDinh <dhang2003@gmail.com>
2025-04-18 06:11:37 +0700

    # initial commit

---
+++ window_function.sql
@@ -0,0 +1,32 @@
+USE AdventureWorkLight1
+-- Công ty đang muốn review lại Revenue lũy tiến của các category theo từng tháng. Bạn hãy giúp team tính toán và tổng hợp view báo cáo này 
+SELECT * From Sales_orders
+SELECT * From Sales_orders_detail
+SELECT * From Sales_orders_reasons
+-- normal join 
+WITH running_revenue_per_cat AS (
+    SELECT 
+        SUM(t1.LineTotal) AS Revenue,
+        cat.ProductCategoryID,
+        cat.ProductCategoryName,
+        MONTH(t2.OrderDate) AS OrderMonth, 
+        YEAR(t2.OrderDate) AS OrderYear
+    FROM Sales_orders_detail t1 
+    LEFT JOIN Sales_orders t2 
+        ON t1.SalesOrderID = t2.SalesOrderID
+    LEFT JOIN Dim_Product t3
+        ON t1.ProductID = t3.ProductID
+    LEFT JOIN Dim_ProductSubCategory t4
+        ON t3.ProductSubcategoryID = CAST(t4.ProductSubcategoryID AS nvarchar)
+    LEFT JOIN Dim_ProductCategory cat 
+        ON cat.ProductCategoryID = t4.ProductCategoryID
+    WHERE t2.OrderDate IS NOT NULL
+    GROUP BY 
+        YEAR(t2.OrderDate), 
+        MONTH(t2.OrderDate), 
+        cat.ProductCategoryID, 
+        cat.ProductCategoryName
+)
+SELECT * FROM running_revenue_per_cat ORDER BY revenue 
+
+-- 
